d56e907da8f221c87679533c3ed483c341fbc8f7
DavidAxbyHome <david.axby@gmail.com>
2017-10-07 15:41:59 +0200

    Added price calculation
    Price calculation added, curretly only working for one product at the time

--- CartScript.js
+++ CartScript.js
@@ -3,13 +3,15 @@ $('.minus-btn').on('click', function (e) {
 	var $this = $(this);
 	var $input = $this.closest('div').find('input');
 	var value = parseInt($input.val());
-
+	var number = parseInt($('.total-price').text());
 		if (value > 1) {
 			value = value - 1;
 		} else {
 			value = 0;
 		}
 	$input.val(value);
+	$(".total-price").html(599*value + "kr");
+	
 
 });
 
@@ -18,14 +20,17 @@ $('.plus-btn').on('click', function (e) {
 	var $this = $(this);
 	var $input = $this.closest('div').find('input');
 	var value = parseInt($input.val());
-
+	var number = parseInt($('.total-price').text());
+	number/=value;
 	if (value < 100) {
 		value = value + 1;
+		
 	} else {
 		value = 100;
 	}
 
 	$input.val(value);
+	$(".total-price").html(599*value + "kr");
 });
 
 $('.like-btn').on('click', function () {
@@ -37,6 +42,9 @@ $('.like-btn').on('click', function () {
 function QuantityCost(){
 	var prodcost = document.getElementById('total-price').getAttribute('value');
 	var prodquant = document.getElementById('total-price').getAttribute('value');
+	prodcost = prodcost * prodquant;
+	return(prodcost);
+	
 	
 	
 }
